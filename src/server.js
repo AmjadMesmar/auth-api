28054a55b3df8f934dc852b9a792356dceab1eeb
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-05-28 17:28:11 +0300

    added v2,again

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/',authRoutes);
 app.use('/api/v1', v1Routes);
-app.use('/api/v2',v2Routes);
+app.use('/api/v2',v2Routes); // new route for fodd and clothes
 app.use(logger);
 
 
